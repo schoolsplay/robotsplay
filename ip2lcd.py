d4ab72537d4e31811ffe798488db00371557e202
Stas Zytkiewicz <stas.zytkiewicz@gmail.com>
2014-07-31 17:38:40 +0200

    Display ip on LCD (used at the end of the boot process)

--- ip2lcd.py
+++ ip2lcd.py
@@ -6,20 +6,25 @@ import time
 __author__ = 'stas Zytkiewicz stas@childsplay.mobi'
 
 from ev3.ev3dev import Lcd
-cmd = subprocess.Popen("ifconfig", shell=True,
+cmd = subprocess.Popen("/sbin/ifconfig", shell=True,
                                    stdout=subprocess.PIPE,
                                    stderr=subprocess.PIPE)
-output = cmd.communicate()[0]
+lines = cmd.communicate()[0].split('\n')
 
+i = 0
+usb_ip = 'Not in use'
+wlan_ip = 'Not in use'
+for line in lines:
+    if 'usb0' in line and 'inet' in lines[i + 1]:
+        usb_ip = lines[i + 1].split('inet addr:')[1].split(' ')[0]
+    if 'wlan0' in line and 'inet' in lines[i + 1]:
+        wlan_ip = lines[i + 1].split('inet addr:')[1].split(' ')[0]
+    i += 1
 
 d = Lcd()
-d.draw.ellipse((20, 20, 60, 60))
+font = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf', 12)
+d.draw.text((10, 10), "Welcome to python-ev3.", font=font)
+d.draw.text((0, 25), "USB IP: %s" % usb_ip, font=font)
+d.draw.text((0, 40), "WLAN IP: %s" % wlan_ip, font=font)
 d.update()
-time.sleep(2)
-d.reset()
-font = ImageFont.load_default()
-d.draw.text((10, 10), "Using IP: %s" % ip, font=font)
-d.update()
-
-
 
